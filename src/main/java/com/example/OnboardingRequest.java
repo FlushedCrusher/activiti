559f3bbea428c482d1e3d6558907212b61456ce5
Daniel Montanez <Daniel.J.W.Montanez@leidos.com>
2017-06-30 14:06:34 -0700

    Walking through the quick start

--- src/main/java/com/example/OnboardingRequest.java
+++ src/main/java/com/example/OnboardingRequest.java
@@ -1,8 +1,16 @@
 package com.example;
 
+import org.activiti.engine.FormService;
+import org.activiti.engine.HistoryService;
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
 import org.activiti.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.runtime.ProcessInstance;
 
 public class OnboardingRequest {
 
@@ -17,6 +25,31 @@ public class OnboardingRequest {
 		String pName = processEngine.getName();
 		String ver = ProcessEngine.VERSION;
 		System.out.println("ProcessEngine [" + pName + "] Version [" + ver + "]");
+		
+		RepositoryService repositoryService = processEngine.getRepositoryService();
+		Deployment deployment = repositoryService.createDeployment()
+				.addClasspathResource("onboarding.bpmn20.xml").deploy();
+		ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery()
+				.deploymentId(deployment.getId()).singleResult();
+		System.out.println(
+				"Found process definition ["
+				+ processDefinition.getName() + "] with id ["
+				+ processDefinition.getId() + "]"
+		);
+		
+		RuntimeService runtimeService = processEngine.getRuntimeService();
+		ProcessInstance processInstance = runtimeService
+				.startProcessInstanceByKey("onboarding");
+		System.out.println(
+				"Onboarding processstarted with process instance id ["
+				+ processInstance.getProcessInstanceId()
+				+ "] key [" + processInstance.getProcessDefinitionKey() + "]"
+		);
+		
+		TaskService taskService = processEngine.getTaskService();
+		FormService formService = processEngine.getFormService();
+		HistoryService historyService = processEngine.getHistoryService();
+		
 	}
 
 }
